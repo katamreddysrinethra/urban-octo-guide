2c778ddae3a2f826aa2e16319573b639b72221cb
katamreddysrinethra <katamreddysrinethra@gmail.com>
2026-06-13 09:45:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-
-st.title("Welcome to Streamlit")
-name = st.text_input("Enter your name")
-if name:
-    st.success(f"Hello {name}")
-
-age = st.slider("Select your age", 1, 100)
-st.write("Age:", age)
-
-if st.button("Celebrate"):
-    st.balloons()
+st.set_page_config(page_title='Attendance')
+st.title('Student Attendance Management System')
+st.write('Use sidebar pages')
